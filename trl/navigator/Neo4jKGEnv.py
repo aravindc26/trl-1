559dd20a8a31d5e764a4ba3a5c406eca5efa320d
Aravind C <aravindc26@gmail.com>
2025-08-07 16:44:39 +0530

    Fixes neo4jkgenv reset func

--- trl/navigator/Neo4jKGEnv.py
+++ trl/navigator/Neo4jKGEnv.py
@@ -22,7 +22,7 @@ class Neo4jKGEnv:
         self.top_k      = top_k
         self.reset_called = False
 
-    def reset(self, initial_prompt: str) -> str:
+    def reset(self, initial_prompt: str):
         self.history, self.trajectory = [], []
         self.curr_node, self.reset_called = None, True
 
@@ -44,10 +44,48 @@ class Neo4jKGEnv:
             return f"{r['id']} | {r['type']} | {r['label']} | score={round(r['score'],3)}"
 
         listing = "\n".join(fmt(x) for x in docs + secs)
-        obs = f"Top candidates (use navigate(id)):\n{listing}"
 
-        self.history.append({"role": "user", "content": obs})
-        return obs
+        prompt = f"""
+        <introduction>
+A user asks a question. Your job is to fetch relevant information to answer the question from a knowledge graph.
+</introduction>
+
+<goal>
+ You will be given a table having headers (score, label, id), that are plausible candidate nodes for exploration. Start by navigating to these nodes using navigation-options. You can only respond with one navigation action, with no other text.
+</goal>
+
+<schema>
+This is the schema for the data inside the node:
+
+- type: either of document or category or section.
+- content: the content of the section, this property is only present in section node.
+- description: the description of the document, holds the purpose of the document.
+- label: Name of the document or category or section.
+- id: id of the node usually of the pattern - category/document#section#<sub-section: optional>
+- links: is a type of array of < id , label >
+
+PS: the nodes are hierarchical, starting from category - document - section - sub-section.
+
+</schema>
+
+<navigation-options>
+You have following options available, as response:
+
+- navigate: respond with navigate(node-id) to get the node's data, you should not come up with your one node data, it should be one of the links or should be one that you have previously visited. Node's data is of the schema - (id, label, content or description and links)
+- stop: respond with stop() when you are done with collecting the context and answer is satisfactory.
+</navigation-options>
+
+<input>
+	<question>{initial_prompt}</question>
+	<starting-nodes>
+        {listing}
+	</starting-nodes>
+</input>
+
+        """
+
+        self.history.append({"role": "user", "content": prompt})
+        return self.history
 
     def navigate(self, node_id: str):
         self._require_reset()
