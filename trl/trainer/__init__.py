ef82263fb7f4fbda1346a71715e3a60eface7816
Aravind C <aravindc26@gmail.com>
2025-08-08 12:50:55 +0530

    Fixes import

--- trl/trainer/__init__.py
+++ trl/trainer/__init__.py
@@ -38,6 +38,7 @@ _import_structure = {
     "gkd_trainer": ["GKDTrainer"],
     "grpo_config": ["GRPOConfig"],
     "grpo_trainer": ["GRPOTrainer"],
+    "offline_multi_turn_grpo_trainer": ["OfflineMultiTurnGRPOTrainer"],
     "iterative_sft_trainer": ["IterativeSFTTrainer"],
     "judges": [
         "AllTrueJudge",
@@ -109,6 +110,7 @@ if TYPE_CHECKING:
     from .gkd_trainer import GKDTrainer
     from .grpo_config import GRPOConfig
     from .grpo_trainer import GRPOTrainer
+    from .offline_multi_turn_grpo_trainer import OfflineMultiTurnGRPOTrainer
     from .iterative_sft_trainer import IterativeSFTTrainer
     from .judges import (
         AllTrueJudge,
